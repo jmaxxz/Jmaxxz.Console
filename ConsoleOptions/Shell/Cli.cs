2050a6b6be9ccbc9144fc88a76bc6c7b9773dd40
Jmaxxz <jmaxxz@jmaxxz.com>
2012-03-02 18:32:44 -0600

    I think forgot to check this in last time.

---
+++ ConsoleOptions/Shell/Cli.cs
@@ -0,0 +1,18 @@
+using System;
+using System.IO;
+
+namespace Jmaxxz.ShellHelpers
+{ 
+    public class Cli
+    {
+        public TextWriter Out { get; private set; }
+        public TextReader In { get;  private set; }
+
+        public Cli(TextReader input, TextWriter output)
+        {
+            Out = output;
+            In = input;
+        }
+    }
+}
+
